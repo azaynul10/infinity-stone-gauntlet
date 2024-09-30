02424fae13042b35109275d4dd3e397241b36a29
Zaynul Abedin Miah <111334392+azaynul10@users.noreply.github.com>
2024-09-30 18:44:09 +0600

    Add files via upload

---
+++ App.js
@@ -0,0 +1,91 @@
+import React, { useRef, useEffect } from 'react';
+// import logo from './logo.svg';
+import * as tf from "@tensorflow/tfjs";
+import * as handpose from "@tensorflow-models/handpose";
+import Webcam from "react-webcam";
+import './App.css';
+import { drawHand } from './utilities';
+
+function App() {
+  const webcamRef = useRef(null);
+  const canvasRef = useRef(null);
+
+  useEffect(() => {
+    const runHandpose = async () => {
+      const net = await handpose.load();
+      console.log("Handpose model loaded.");
+      setInterval(() => {
+        detect(net);
+      }, 100);
+    };
+
+    runHandpose();
+  }, []);
+
+  const detect = async (net) => {
+    if (
+      typeof webcamRef.current !== "undefined" &&
+      webcamRef.current !== null &&
+      webcamRef.current.video.readyState === 4
+    ) {
+      // Get Video Properties
+      const video = webcamRef.current.video;
+      const videoWidth = webcamRef.current.video.videoWidth;
+      const videoHeight = webcamRef.current.video.videoHeight;
+
+      // Set video width
+      webcamRef.current.video.width = videoWidth;
+      webcamRef.current.video.height = videoHeight;
+
+      // Set canvas height and width
+      canvasRef.current.width = videoWidth;
+      canvasRef.current.height = videoHeight;
+
+      // Make Detections
+      const hand = await net.estimateHands(video);
+      console.log(hand);
+
+      // Draw mesh
+      const ctx = canvasRef.current.getContext("2d");
+      ctx.clearRect(0, 0, videoWidth, videoHeight); // Clear previous drawings
+      drawHand(hand, ctx);
+    }
+  };
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <Webcam
+          ref={webcamRef}
+          style={{
+            position: "absolute",
+            marginLeft: "auto",
+            marginRight: "auto",
+            left: 0,
+            right: 0,
+            textAlign: "center",
+            zindex: 9,
+            width: 640,
+            height: 480
+          }}
+        />
+        <canvas
+          ref={canvasRef}
+          style={{
+            position: "absolute",
+            marginLeft: "auto",
+            marginRight: "auto",
+            left: 0,
+            right: 0,
+            textAlign: "center",
+            zindex: 9,
+            width: 640,
+            height: 480
+          }}
+        />
+      </header>
+    </div>
+  );
+}
+
+export default App;
